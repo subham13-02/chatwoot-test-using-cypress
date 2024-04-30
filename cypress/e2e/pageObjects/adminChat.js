64b787a151dce18b978dda46224da2765eb373b3
subham13-20 <sahu21019@gmail.com>
2024-04-30 13:16:07 +0530

    verify private test

--- cypress/e2e/pageObjects/adminChat.js
+++ cypress/e2e/pageObjects/adminChat.js
@@ -7,7 +7,8 @@ const conversationLocators = {
     latestMessageDots: '.conversation-panel li:last-child button[type="submit"]',
     deleteCurrentMessage: "//p[normalize-space()='Delete']", //using xpath is the only way I can find to locate delete btn.
     currentMessage: '.conversation-panel li:last-child',                                      
-    confirmDelete: "button[class='button action-button smooth alert'][type='submit']"
+    confirmDelete: "button[class='button action-button smooth alert'][type='submit']",
+    privateChatSection:'[data-placeholder="Shift + enter for new line. This will be visible only to Agents"]',
 };
 
 class AdminChat {
@@ -22,10 +23,17 @@ class AdminChat {
     clickFirstConversationUser() {
         cy.contains('h4', conversationLocators.firstConversationUser).click();
     }
+    checkMessageExist(expectedMessage){
+        cy.contains(expectedMessage).should('exist');
+    }
 
     writeMessage(message) {
-        cy.log(message);
         cy.get(conversationLocators.chatSection).type(message);
+    }
+    writePrivateMessage(message){
+        cy.get('button span').contains('Private Note').click();
+        cy.get(conversationLocators.privateChatSection).type(message);
+        cy.get('button').contains('Add Note (⌘ + ↵)').click();
     }
 
     clickOnSendMessage() {
@@ -43,6 +51,7 @@ class AdminChat {
         cy.get('p').contains('Delete').click();              
         cy.get(conversationLocators.confirmDelete).click();
     }
+
 }
 
 export default AdminChat;
--- cypress/e2e/specs/CheckMessage.cy.js
+++ cypress/e2e/specs/CheckMessage.cy.js
@@ -31,52 +31,68 @@ describe('Specs for Chatwoot dashboard', () => {
         adminChat.writeMessage(longMessage);
         adminChat.clickOnSendMessage();
         cy.contains(longMessage).should('exist');
-        
-        adminChat.deleteLatestMessage();
-        // adminChat.deleteAnyMessage('subham');
-        
+        adminChat.deleteLatestMessage();        
         adminChat.writeMessage(sortMessage);
         adminChat.clickOnSendMessage();
-        cy.contains(sortMessage).should('exist');
+        adminChat.checkMessage(sortMessage);
+        
+    });
+
+    it('send private message from admin side and delete that message', () => {
+        const sortMessage = faker.lorem.words(2);
+        const adminChat = new AdminChat();
+        cy.visit('/');
+        adminChat.clickConversationIcon();
+        adminChat.clickFirstConversationUser();
+        adminChat.writePrivateMessage(sortMessage)
+        adminChat.checkMessageExist(sortMessage);
+        adminChat.deleteLatestMessage();
+    });
+    it('Verify assigned agent and unassigned agest', ()=>{
+        const adminChat = new AdminChat();
+        cy.visit('/');
+        adminChat.clickConversationIcon();
+        adminChat.clickFirstConversationUser();
     })
+
+
 
     //############## Iframe of user side Chat is not accessable. ###############
 
-    // it('send message from user side', () => {
-    //     const userChat = new UserChat();
-    //     cy.visit('https://subham13-02.github.io/chat-user-pages/');
-    //     cy.get('button[title="Open chat window"]').click();
-        // cy.get('#chatwoot_live_chat_widget').then(function($iframe){
-        //     let iframebody = $iframe.contents().find('body')
-        //     cy.wrap(iframebody).contains("Continue conversation").click();            
-        // });
-        
-        // cy.iframe('button[title="Open chat window"]').within(()=>{
-        //     cy.contains("Continue conversation").click();  
+        // it('send message from user side', () => {
+        //     const userChat = new UserChat();
+        //     cy.visit('https://subham13-02.github.io/chat-user-pages/');
+        //     cy.get('button[title="Open chat window"]').click();
+            // cy.get('#chatwoot_live_chat_widget').then(function($iframe){
+            //     let iframebody = $iframe.contents().find('body')
+            //     cy.wrap(iframebody).contains("Continue conversation").click();            
+            // });
+            
+            // cy.iframe('button[title="Open chat window"]').within(()=>{
+            //     cy.contains("Continue conversation").click();  
+            // })
+            // cy.get('button[title="Open chat window"]').its().then(cy.wrap).find('button')
+                    
+            // const iframe = cy.get('#chatwoot_live_chat_widget')
+            //     .its('0.contentDocument.body')
+            //     .should('be.visible')
+            //     .then(cy.wrap)
+            // iframe.get('button:nth-child(1)').click();     
         // })
-        // cy.get('button[title="Open chat window"]').its().then(cy.wrap).find('button')
-             
-        // const iframe = cy.get('#chatwoot_live_chat_widget')
-        //     .its('0.contentDocument.body')
-        //     .should('be.visible')
-        //     .then(cy.wrap)
-        // iframe.get('button:nth-child(1)').click();     
-    // })
-    // it.only('gets the post', () => {cy.visit('https://subham13-02.github.io/chat-user-pages/')
-    //     const getIframeDocument = () => {
-    //         return cy.get('#chatwoot_live_chat_widget"]')
-    //             .its('0.contentDocument').should('exist')   
-    //     }
-    //     const getIframeBody = () => {
-    //         // get the document
-    //         return getIframeDocument()
-    //         // automatically retries until body is loaded
-    //         .its('body').should('not.be.undefined')
-    //         // wraps "body" DOM element to allow
-    //         // chaining more Cypress commands, like ".find(...)"
-    //         .then(cy.wrap)
-    //     }
+        // it.only('gets the post', () => {cy.visit('https://subham13-02.github.io/chat-user-pages/')
+        //     const getIframeDocument = () => {
+        //         return cy.get('#chatwoot_live_chat_widget"]')
+        //             .its('0.contentDocument').should('exist')   
+        //     }
+        //     const getIframeBody = () => {
+                // get the document
+        //         return getIframeDocument()
+                // automatically retries until body is loaded
+        //         .its('body').should('not.be.undefined')
+                // chaining more Cypress commands, like ".find(...)"
+        //         .then(cy.wrap)
+        //     }
 
-    //     getIframeBody().find('button').should('have.text', 'Continue conversation').click()
-    // })
+        //     getIframeBody().find('button').should('have.text', 'Continue conversation').click()
+        // })
 })
